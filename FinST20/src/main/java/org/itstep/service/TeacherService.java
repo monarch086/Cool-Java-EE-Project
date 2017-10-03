e5292c9f5affb3ac0367c95c5b16b88b0baf4287
student <student@K03-11.lan.itstep.kiev.ua>
2017-10-03 20:28:46 +0300

    Zoya

--- FinST20/src/main/java/org/itstep/service/TeacherService.java
+++ FinST20/src/main/java/org/itstep/service/TeacherService.java
@@ -5,8 +5,8 @@ import org.itstep.dao.pojo.Teacher;
 public interface TeacherService {
 
 	public Teacher getTeacher(String login);
-	
+
 	public Teacher createAndUpdateTeacher(Teacher teacher);
-	
+
 	public void deleteTeacher(Teacher teacher);
 }
